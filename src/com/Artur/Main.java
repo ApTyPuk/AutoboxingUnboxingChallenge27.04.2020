fd78d37ba92787b8755ce625aff80e06a93dcd85
ApTuPuK <kpackalv@yahoo.com>
2020-04-28 21:27:56 +0300

    main update2

--- src/com/Artur/Main.java
+++ src/com/Artur/Main.java
@@ -8,10 +8,20 @@ public class Main {
 	//This is a test to check how git hub works
 
 
-            public Customers(String branchName, String name) {
-            super(branchName);
-            this.name = name;
-            this.transactions = new ArrayList<Double>();
+        private void addBranch(){ // Customers are attached to the branch. Transactions attached to customer
+
+        }
+
+        private void addCustomerToCertainBranch(){ //+initial a transaction same time
+
+        }
+
+        private void newTransactionForExistingCustomer(){ //unlimited time of transactions
+
+        }
+
+        public void showListOfCustomersForCertainBranch(){  //+list of their transactions
+
         }
 
     }
