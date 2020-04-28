8c8354726bb461a70e344291bf89fd8630d70f8c
ApTuPuK <kpackalv@yahoo.com>
2020-04-28 21:18:27 +0300

    main update1

--- src/com/Artur/Main.java
+++ src/com/Artur/Main.java
@@ -1,8 +1,18 @@
 package com.Artur;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	//This is a test to check how git hub works
+
+
+            public Customers(String branchName, String name) {
+            super(branchName);
+            this.name = name;
+            this.transactions = new ArrayList<Double>();
+        }
+
     }
 }
